180ef15dfbd92933e774855a58e9d713a5747a3a
zabealbe <zabeoalberto99@gmail.com>
2020-10-29 12:38:13 +0100

    added support during network merging for messages with same name across different networks if they are identical

--- flatbuf-schema-generator/lib/network.py
+++ flatbuf-schema-generator/lib/network.py
@@ -6,6 +6,7 @@ MESSAGES_FILE = "messages.json"
 class Network:
     def __init__(self, path):
         self.path = path
+        self.name = path.split('/')[-1]
         self.contents = []
         if path:
             self.load()
@@ -22,9 +23,22 @@ class Network:
 
     def merge_with(self, network):
         if isinstance(network, Network):
+            for m1 in network.contents:
+                m2 = self.get_message_by_name(m1['name'])
+                if m2:  # message name conflict
+                    print("Message with same name {0} found in networks {1} and {2}"
+                          .format(m1['name'], self.name, network.name))
+                    print("Checking if the two messages are actually the same message...")
+                    for (_, m1value), (_, m2value) in zip(m1.items(), m2.items()):
+                        # check if messages are actually the same one
+                        if m1value != m2value:
+                            raise Exception("Found two incompatible messages with same name {0} in networks {1}, {2}"
+                                            .format(m1['name'], self.name, network.name))
+                    print("Messages found to be the same, the merge will continue")
             self.contents += network.contents
         else:
             raise Exception("Parameter network isn't a Network instance")
+        self.name = self.name + network.name
 
     def get_all_messages(self):
         return self.contents
