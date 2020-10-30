28446c0e6d897b71337233d230a5e9c7ead71d75
zabealbe <zabeoalberto99@gmail.com>
2020-10-30 01:04:40 +0100

    Added comments

--- includes-generator/config.py
+++ includes-generator/config.py
@@ -1,8 +1,9 @@
-OUTPUT_DIR = "./out"
-
+# INPUT FILES
 IDS_FILE = "../id-generator/out/ids.json"
 FLATBUF_SCHEMA_FILE = "../flatbuf-schema-generator/out/schema.fbs"
 
+# OUTPUT FILES
+OUTPUT_DIR = "./out"
 # IDS & masks
 C_INCLUDE = "out/ids.h"
 PY_INCLUDE = "out/ids.py"
