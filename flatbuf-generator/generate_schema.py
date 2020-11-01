1b30a1bbf4eac36ed9c24690efa4d56545bf50ef
zabealbe <zabeoalberto99@gmail.com>
2020-11-01 14:24:06 +0100

    added enums deduplication

--- flatbuf-generator/generate_schema.py
+++ flatbuf-generator/generate_schema.py
@@ -17,6 +17,36 @@ def get_message_schema(message):
     return schema
 
 
+def get_schema(messages):
+    schema = ""
+    for m in messages:
+        schema += "{0}\n".format(get_message_schema(m))
+
+    stripped_schema = ""
+    enums = {}
+
+    for line in schema.splitlines():  # enums deduplication
+        if "enum" not in line:
+            stripped_schema += line + "\n"
+            continue
+
+        enum_name = line.split(" ")[1]
+        enum_body = line.split("{")[1].split("}")[0]
+        if enum_name in enums.keys():
+            if enum_body != enums[enum_name]:
+                raise Exception(
+                    "You can't use the same name for different enums:"
+                    "\nNAME: {0}\nENUMS:"
+                    "\n\t{1}\n\t{2}"
+                    .format(enum_name, enum_body, enums[enum_name])
+                )
+        else:
+            enums[enum_name] = enum_body
+            stripped_schema += line + "\n"
+
+    return stripped_schema
+
+
 def main():
     merge_mode = False
 
@@ -29,21 +59,18 @@ def main():
         else:
             networks.append(Network(path, network_name))
         print("Loaded {0}".format(network_name))
-    print("")
 
     for n in networks:
-        schema = ""
-        for m in n.get_all_messages():
-            schema += "{0}\n".format(get_message_schema(m))
-
-        print("====== Schema generating ======")
+        schema = get_schema(n.get_all_messages())
+        print("")
+        print("====== Schema for generating ======")
         print("Schema generated successfully!")
-        output_path = config.OUTPUT_FILE.replace("[network]", n.name)
+        output_path = config.SCHEMA_FILE.replace("[network]", n.name)
         print("Saving schema to {0}".format(output_path))
         create_file_subtree(output_path)
         with open(output_path, "w+") as f:
             print(schema, file=f)
-        print("====== Done! ======")
+        print("done.")
 
 
 if __name__ == "__main__":
