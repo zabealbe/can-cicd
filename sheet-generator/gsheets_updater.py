ca2df5b44c93c7154c31cf531a179e9bec3845b5
Alberto Zabeo <zabeoalberto99@gmail.com>
2021-03-13 11:07:50 +0100

    idk

--- sheet-generator/gsheets_updater.py
+++ sheet-generator/gsheets_updater.py
@@ -1,9 +1,7 @@
 from __future__ import print_function
 import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -17,40 +15,16 @@ def push_csv():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    scope = ["https://www.googleapis.com/auth/spreadsheets"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    client = gspread.authorize(credentials)
 
-    service = build('sheets', 'v4', credentials=creds)
-
+    spreadsheet = client.open_by_key(SAMPLE_SPREADSHEET_ID)
+    
     # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
-
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
-
+    with open('out/messages.csv', 'r') as file_obj:
+        content = file_obj.read()
+        client.import_csv(SAMPLE_SPREADSHEET_ID, data=content)
 
 if __name__ == '__main__':
     push_csv()
