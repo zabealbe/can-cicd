fb2a86f90531e3680cb3022f9129e1988c5fd6ec
Alberto Zabeo <zabeoalberto99@gmail.com>
2021-03-15 16:53:07 +0100

    fix(sheet-generator): fixed authentication for edit authorizations for gsheets

--- sheet-generator/gsheets_updater.py
+++ sheet-generator/gsheets_updater.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os.path
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from config import config as c
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -15,16 +16,14 @@ def push_csv():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
-    scope = ["https://www.googleapis.com/auth/spreadsheets"]
-    credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
-    client = gspread.authorize(credentials)
+    gc = gspread.service_account(filename="credentials.json")
 
-    spreadsheet = client.open_by_key(SAMPLE_SPREADSHEET_ID)
+    spreadsheet = gc.open_by_key(SAMPLE_SPREADSHEET_ID)
     
     # Call the Sheets API
-    with open('out/messages.csv', 'r') as file_obj:
+    with open(c.CSV_FILE, 'r') as file_obj:
         content = file_obj.read()
-        client.import_csv(SAMPLE_SPREADSHEET_ID, data=content)
+        gc.import_csv(SAMPLE_SPREADSHEET_ID, data=content)
 
 if __name__ == '__main__':
     push_csv()
