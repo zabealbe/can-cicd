d41144c4cf9ccbac8e03e6fb980e92c3046ab0aa
Alberto Zabeo <zabeoalberto99@gmail.com>
2021-03-17 15:31:08 +0100

    feat(includes-generator): added python support

---
+++ includes-generator/generators/py_gen.py
@@ -0,0 +1,35 @@
+from config import config as c
+
+
+def generate_ids_include(ids, network_version):
+    header = ""
+    header += "NETWORK_VERSION = {0}\n\n".format(network_version)
+    for t in ids:
+        header += "# TOPIC {0}\n".format(t['topic'])
+        header += "TOPIC_{0}_MASK = 0b{1:>011b}\n".format(t['topic'], 0b00000011111)
+        header += "TOPIC_{0}_FILTER = 0b{1:>011b}\n".format(t['topic'], t['id'])
+        for m, mid in t['messages'].items():
+            header += "{0} = 0b{1:>011b}\n".format(m, mid)
+        header += "\n"
+
+    return header
+
+
+def generate_flatbuf_include(flatbuf_schema):
+    output = ""
+    return output
+
+
+def generate_canconfig_include(canconfig, canconfig_version):
+    header = ""
+    header += "CANCONFIG_VERSION = {0}\n\n".format(canconfig_version)
+    for k, v in canconfig.items():
+        if isinstance(v, dict):
+            header += "\n"
+            for _k, _v in v.items():
+                header += f"{_k.upper()} = {_v}"
+                header += "\n"
+        else:
+            header += f"{k.upper()} = {v}\n"
+
+    return header
--- includes-generator/main.py
+++ includes-generator/main.py
@@ -1,15 +1,17 @@
 from lib.utils import *
-from cgen import generator as cgen
-from pygen import generator as pygen
+from generators import c_gen
+from generators import py_gen
 from config import config as c
 
 
 def generate_id_includes(message_ids, network_version, output_path):
-    cgenerated = cgen.generate_ids_include(message_ids, network_version)
-    with open(output_path, "w+") as f:
+    cgenerated = c_gen.generate_ids_include(message_ids, network_version)
+    with open(f"{output_path}{c.C_IDS_INCLUDE}", "w+") as f:
         print(cgenerated, file=f)
 
-    pygenerated = pygen.generate_ids_include(message_ids, network_version)
+    pygenerated = py_gen.generate_ids_include(message_ids, network_version)
+    with open(f"{output_path}{c.PY_IDS_INCLUDE}", "w+") as f:
+        print(pygenerated, file=f)
 
 
 def generate_flatbuf_includes(flatbuf_schema):
@@ -17,12 +19,14 @@ def generate_flatbuf_includes(flatbuf_schema):
 
 
 def generate_canconfig_includes(canconfig, canconfig_version, output_path):
-    cgenerated = cgen.generate_canconfig_include(canconfig, canconfig_version)
-    with open(output_path, "w+") as f:
+    cgenerated = c_gen.generate_canconfig_include(canconfig, canconfig_version)
+    with open(f"{output_path}{c.C_CANCONFIG_INCLUDE}", "w+") as f:
         print(cgenerated, file=f)
 
-    pygenerated = pygen.generate_ids_include(canconfig, canconfig_version)
-
+    pygenerated = py_gen.generate_canconfig_include(canconfig, canconfig_version)
+    with open(f"{output_path}{c.PY_CANCONFIG_INCLUDE}", "w+") as f:
+        print(pygenerated, file=f)
+        
 
 def main():
     # Config validity checks
@@ -30,9 +34,10 @@ def main():
         print("OUTPUT_DIR in config.py isn't a valid directory path")
         print("examples:\n\t./path/to/directory/\n\tpath/to/directory/")
         exit(1)
-    
+
     # IDs & masks
-    for n, path in parse_network_multipath(c.IDS_FILE).items():
+    for network_name, path in parse_network_multipath(c.IDS_FILE).items():
+        output_path = c.OUTPUT_DIR.replace("[network]", network_name)
         print(path)
         
         ids_file = load_json(path)
@@ -40,15 +45,14 @@ def main():
         network_version = float(ids_file["network_version"])
         print(f"Loaded message ids from {path}")
         
-        output_path = c.OUTPUT_DIR.replace("[network]", n)
         create_subtree(output_path)
         
-        output_ids_file = f"{output_path}/{c.C_IDS_INCLUDE}"
-        generate_id_includes(ids, network_version, output_ids_file)
-        print(f"Generated id includes for {n} in {output_ids_file}\n")
+        generate_id_includes(ids, network_version, output_path)
+        print(f"Generated id includes in {output_path}\n")
 
     # CAN config
-    for n, path in parse_network_multipath(c.CANCONFIG_FILE).items():
+    for network_name, path in parse_network_multipath(c.CANCONFIG_FILE).items():
+        output_path = c.OUTPUT_DIR.replace("[network]", network_name)
         print(path)
 
         canconfig_file = load_json(path, c.CANCONFIG_FILE_VALIDATION_SCHEMA)
@@ -56,12 +60,10 @@ def main():
         canconfig_version = float(canconfig_file["canconfig_version"])
         print(f"Loaded can configuration from {path}")
 
-        output_path = c.OUTPUT_DIR.replace("[network]", n)
         create_subtree(output_path)
 
-        output_canconfig_file = f"{output_path}/{c.C_CANCONFIG_INCLUDE}"
-        generate_canconfig_includes(canconfig, canconfig_version, output_canconfig_file)
-        print(f"Generated canconfig includes for {n} in {output_canconfig_file}\n")
+        generate_canconfig_includes(canconfig, canconfig_version, output_path)
+        print(f"Generated canconfig includes in {output_path}\n")
 
     '''
         with open(config.FLATBUF_SCHEMA_FILE) as flatbuf_schema:
